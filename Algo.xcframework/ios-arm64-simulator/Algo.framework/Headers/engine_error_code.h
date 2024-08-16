02bb6e55992669b5c14cd734b7e02dea1bb3f58e
AuthMe-Team <dev@authme.com>
2024-08-16 02:23:26 +0000

    Release AuthMe 2.7.23

---
+++ Algo.xcframework/ios-arm64-simulator/Algo.framework/Headers/engine_error_code.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum _E_AUTHME_ENGINE_RETUEN_CODE : int
+{
+    eAuthMe_Engine_Success = 0,
+    eAuthMe_Engine_Failed,
+    eAuthMe_Engine_VersionNotSupported,
+    eAuthMe_Engine_Authorization_Pass,
+    eAuthMe_Engine_Authorization_Failed,
+    eAuthMe_Engine_ReadFileFailed,
+    eAuthMe_Engine_InvalidParams,
+    eAuthMe_Engine_InvalidStages,
+    eAuthMe_Engine_MissingConfig,
+    eAuthMe_Engine_InvalidUIParams,
+} EAuthMeEngineReturnCode;
+
+#ifdef __cplusplus
+}
+#endif
---
+++ AuthMe.xcframework/ios-arm64-simulator/AuthMe.framework/PrivateHeaders/AuthMe_Private.h
@@ -0,0 +1,14 @@
+//
+//  AuthMePrivate.h
+//  AuthMe
+//
+//  Created by Arthur Kao on 2022/3/10.
+//
+
+#ifndef AuthMe_Private_h
+#define AuthMe_Private_h
+
+#import "ImageConverter.h"
+#import "EngineWrapper.h"
+
+#endif /* AuthMe_Private_h */
