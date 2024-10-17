4ef8c5696fcabb3f03068b986114e145cde959d1
AuthMe-Team <dev@authme.com>
2024-10-17 03:30:04 +0000

    Release AuthMe 2.7.32

---
+++ Algo.xcframework/ios-arm64_x86_64-simulator/Algo.framework/Headers/card_classification.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "engine_error_code.h"
+#include "engine_type.h"
+#include "engine_base.h"
+
+EAuthMeEngineReturnCode AuthMe_CardClassification_Create(long* pHandle);
+
+EAuthMeEngineReturnCode AuthMe_CardClassification_Initial(long handle, const char* szModelPath);
+
+const char* AuthMe_CardClassification_GetModelInfo(long handle);
+
+EAuthMeEngineReturnCode AuthMe_CardClassification_SetThreshold(long handle, const float fThreshold);
+
+float AuthMe_CardClassification_GetThreshold(long handle);
+
+EAuthMeEngineReturnCode AuthMe_CardClassification_Run(long handle, AuthMeImage* pImage, int* pPoints, int iLength, EAuthMeCardClass* peClass, AuthMeEngineDebugInfo* pDebugInfo);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ AuthMe.xcframework/ios-arm64_x86_64-simulator/AuthMe.framework/Headers/AuthMe.h
@@ -0,0 +1,19 @@
+//
+//  AuthMe.h
+//  AuthMe
+//
+//  Created by Mikimoto on 2021/8/21.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for AuthMe.
+FOUNDATION_EXPORT double AuthMeVersionNumber;
+
+//! Project version string for AuthMe.
+FOUNDATION_EXPORT const unsigned char AuthMeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthMe/PublicHeader.h>
+
+
